1515f604b1b19bd4e592dcb67bcf5b6b057e3cbe
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-09-30 23:38:38 -0500

    Added improvements to getting the topics from the kafka broker. Need to figure out how to split a stream sanely

--- Beem/src/main/java/SchoolToCityStream.java
+++ Beem/src/main/java/SchoolToCityStream.java
@@ -1,13 +1,12 @@
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.util.JSONPObject;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.json.*;
+import org.json.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Predicate;
@@ -33,13 +32,16 @@ public class SchoolToCityStream
     private String cityBrokerAddress;
     private HashMap<String, String> cities; // used to keep the cities we have in our database and their topic names
     private Map<String, List<PartitionInfo>> schoolKTopics;
-    private Map<String, List<PartitionInfo>> cityKTopics;
+    private Map<String, List<PartitionInfo>> brokerKTopics;
+    private Map<String, List<PartitionInfo>> cityDeliveryKTopics;
+    private Map<String, List<PartitionInfo>> cityRideShareKTopics;
     private StreamsBuilder builder;
 
     private KStream<String, String> sourceStream;
     private KStream<String, String> deliveryStream;
     private KStream<String, String> rideShareStream;
-    private KStream<String, String>[] cityStreams; // array of streams - one stream for each city
+    private KStream<String, String>[] cityDeliveryStreams; // array of delivery streams - one stream for each city
+    private KStream<String, String>[] cityRideShareStream; // array of ride share streams - one stream for each city
 
     public static void main(String[] args)
     {
@@ -79,7 +81,9 @@ public class SchoolToCityStream
         this.cityBrokerAddress = cityBrokerAddress;
 
         setSchoolTopic(schoolBrokerAddress, schoolTopic);
-        this.cityKTopics = getKTopics(cityBrokerAddress);
+        this.brokerKTopics = getKTopics(cityBrokerAddress);
+        this.cityDeliveryKTopics = getDeliveryTopics(this.brokerKTopics);
+        this.cityRideShareKTopics = getRideShareTopics(this.brokerKTopics);
 
         this.builder = new StreamsBuilder();
 
@@ -104,7 +108,7 @@ public class SchoolToCityStream
                     @Override
                     public boolean test(String key, String value)
                     {
-                        JSONObject json = (JSONPObject) new JSONParser().parse(value);
+                        JSONObject json = new JSONObject(value);
                         String jobType = json.get("job_type").toString();
 
                         if(jobType.equalsIgnoreCase("delivery"))
@@ -120,7 +124,7 @@ public class SchoolToCityStream
                     @Override
                     public boolean test(String key, String value)
                     {
-                        JSONObject json = (JSONPObject) new JSONParser().parse(value);
+                        JSONObject json = new JSONObject(value);
                         String jobType = json.get("job_type").toString();
 
                         if(jobType.equalsIgnoreCase("rideShare"))
@@ -132,6 +136,19 @@ public class SchoolToCityStream
         );
     }
 
+    //TODO: FIGURE OUT HOW TO FUCKING BRANCH THESE EVENTS IN A SANE MANNER
+//    /**
+//     * How to branch streams is found here:
+//     * @see 'https://kafka.apache.org/20/documentation/streams/developer-guide/dsl-api.html#transform-a-stream'
+//     * @see 'https://kafka-tutorials.confluent.io/split-a-stream-of-events-into-substreams/kstreams.html'
+//     */
+//    public void setUpCityStreams()
+//    {
+//        this.cityDeliveryStreams = this.deliveryStream.branch(
+//                new Ks
+//        );
+//    }
+
 
 
     /**
@@ -140,7 +157,7 @@ public class SchoolToCityStream
      * @param schoolTopic The topic we need to verify actually exists before pulling data from it.
      * @throws NoSuchElementException Throws if topic does not exist at the specified broker.
      */
-    public void setSchoolTopic(String brokerAddress,String schoolTopic) throws NoSuchElementException
+    public void setSchoolTopic(String brokerAddress, String schoolTopic) throws NoSuchElementException
     {
         Map<String, List<PartitionInfo>> existingTopics = getKTopics(brokerAddress);
 
@@ -165,7 +182,10 @@ public class SchoolToCityStream
 
     /**
      * Need to get the list of topics from the kafka broker so we can verify that the topic the user wants
-     * to stream from actually exists.
+     * stream from actually exists.
+     * @param brokerAddress The kafka broker that has the city topic we want. Ex - Ex. - 'ipaddress:portNum'.
+     * @return A map of the topics. The key is the topic name, the PartitionInfo has the topic name, number of partitions,
+     *      the leader, number of replicas, the isr, and info on offline replicas
      */
     public Map<String, List<PartitionInfo>> getKTopics(String brokerAddress)
     {
@@ -176,13 +196,104 @@ public class SchoolToCityStream
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        Map<String, List<PartitionInfo>> topics = consumer.listTopics();
+
         consumer.close();
 
-        return consumer.listTopics();
+        return topics;
+    }
+
+    /**
+     * Used to segregate the topics into a delivery topic map for quick look up and categorization of the
+     * messages into their respective streams.
+     * @param brokerTopics All of the topics that we pulled from the broker.
+     * @return All of the city delivery topics we will need to send our new streams to. The key is the 'city-state'.
+     *      The rest of the topic information, like the actual topic name is in the PartitionInfo list, the value
+     *      of the map.
+     */
+    protected Map<String, List<PartitionInfo>> getDeliveryTopics(Map<String, List<PartitionInfo>> brokerTopics)
+    {
+        Map<String, List<PartitionInfo>> topics = new HashMap<>();
+
+        for (Map.Entry<String, List<PartitionInfo>> entry : brokerTopics.entrySet())
+        {
+            // Format for city topics is 'city-state-job'
+            String city, state, job;
+            String[] key = entry.getKey().split("-");
+
+            if (key.length < 3) // name not long enough so can't be valid topic
+                continue;
+
+            job = key[2];
+            if(!isValidJob(job))
+                continue; // not a valid job so go to next one
+
+            city = key[0];
+            state = key[1];
+
+
+            if(job.equalsIgnoreCase("delivery"))
+                topics.put((city + "-" + state), entry.getValue());
+        }
+
+        return topics;
+    }
+
+    /**
+     * Used to segregate the topics into a delivery topic map for quick look up and categorization of the
+     * messages into their respective streams.
+     * @param brokerTopics All of the topics that we pulled from the broker.
+     * @return All of the rideshare city topics we will need to send our new streams to. The key is the 'city-state'.
+     *      The rest of the topic information, like the actual topic name is in the PartitionInfo list, the value
+     *      of the map.
+     */
+    protected Map<String, List<PartitionInfo>> getRideShareTopics(Map<String, List<PartitionInfo>> brokerTopics)
+    {
+        Map<String, List<PartitionInfo>> topics = new HashMap<>();
+        String city, state, job;
+
+        for (Map.Entry<String, List<PartitionInfo>> entry : brokerTopics.entrySet())
+        {
+            // Format for city topics is 'city-state-job'
+            String[] key = entry.getKey().split("-");
+
+            if (key.length < 3) // name not long enough so can't be valid topic
+                continue;
+
+            job = key[2];
+            if(!isValidJob(job))
+                continue; // not a valid job so go to next one
+
+            city = key[0];
+            state = key[1];
+
+
+            if(job.equalsIgnoreCase("rideshare"))
+                topics.put((city + "-" + state), entry.getValue());
+        }
+
+        return topics;
+    }
+
+    /**
+     * Helper function to help is determine if a job is a valid one. Need this for looking at topics to ensure that
+     * we are storing actual city topics instead of school topics.
+     * @param job The job we are trying to validate. Ex - delivery
+     * @return True if the job string is a valid job type.
+     */
+    private boolean isValidJob(String job)
+    {
+        if (job.equalsIgnoreCase("delivery") || job.equalsIgnoreCase("rideshare"))
+            return true;
+        return false;
     }
 
 
-
+    /**
+     * Create the properties for this streaming application. This is by default setup for only looking
+     * at school topics in Texas.
+     * @return The configuration properties to connect to our brokers.
+     */
     public Properties createProps()
     {
         Properties props = new Properties();
